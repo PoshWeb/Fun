8d0a4760c7d02c6c0995522e489cbc167641adca
James Brundage <+@noreply.github.com>
2026-06-18 00:25:50 -0700

    test(server): Adding tests for query, formdata, and json data

--- Fun.tests.ps1
+++ Fun.tests.ps1
@@ -1,9 +1,46 @@
 describe Fun {
     it 'Is Fun To Make a Server' {
         function / { "Hello from Fun" }
+        $job = Start-Fun
+        
+        Invoke-RestMethod $job.Name | 
+            Should -Be "Hello from Fun"
+        
+        $job.HttpListener.Stop()
+    }
 
+    it 'Is easy to map query strings' {
+
+        function / { param([int]$Number) $Number }
+
+        $randomNumber = Get-Random
+        $job = Start-Fun
         Invoke-RestMethod (
-            Start-Fun | Select-Object -ExpandProperty Name
-        ) | Should -Be "Hello from Fun"
+            "$($job.Name)/?number=$randomNumber"
+        ) | Should -Be "$randomNumber"
+    }
+
+    it 'Can map form data' {
+        function / { param([int]$Number) $Number }
+
+        $randomNumber = Get-Random
+        $job = Start-Fun
+        Invoke-RestMethod $job.Name -Method POST -Body "number=$randomNumber" -ContentType (
+            'application/x-www-form-urlencoded'
+        )  | Should -Be "$randomNumber"
+        $job.HttpListener.Stop()
+    }
+
+    it 'Can map json data' {
+        function / { param([int]$Number) $Number }
+
+        $randomNumber = Get-Random
+        $job = Start-Fun
+        Invoke-RestMethod $job.Name -Method POST -Body (
+            @{number=$randomNumber} | ConvertTo-Json
+        ) -ContentType (
+            'application/json'
+        )  | Should -Be "$randomNumber"
+        $job.HttpListener.Stop()
     }
 }
