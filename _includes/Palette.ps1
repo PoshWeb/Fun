9a587031f04e017ce731b682cb6db55af51b025f
James Brundage <+@noreply.github.com>
2026-06-21 14:08:25 -0700

    feat(website): `/_includes/Palette` ( Fixes #44 )
    Adding include for palette selection

---
+++ _includes/Palette.ps1
@@ -0,0 +1,112 @@
+<#
+.SYNOPSIS
+    Includes a palette selector
+.DESCRIPTION
+    Includes a palette selector and randomized palette switcher.
+    
+    This allows the page to use multiple color palettes.
+#>
+param(
+# The source for all palette information
+[uri]
+$AllPalettesSource = 'https://4bitcss.com/Palettes.json',
+
+# The Palette CDN.  This is the root URL of all palettes.
+[uri]
+$PaletteCDN = 'https://cdn.jsdelivr.net/gh/2bitdesigns/4bitcss@latest/css/',
+
+# The identifier for the palette `<select>`.
+[string]
+$SelectPaletteId = 'SelectPalette',
+
+# The identifier for the stylesheet.  By default, palette.
+[string]
+$PaletteId = 'palette',
+
+[string]
+$DefaultPalette = $(
+    if ($page.Palette) {
+        $page.Palette
+    } elseif ($site.Palette) {
+        $site.Palette
+    }
+    else {
+        ''
+    }
+)
+)
+
+
+$JavaScript = @"
+function SetPalette(event) {
+    var palette = document.getElementById('$PaletteId')
+    if (! palette) {
+        palette = document.createElement('link')
+        palette.rel = 'stylesheet'
+        palette.id = 'palette'
+        document.head.appendChild(palette)
+    }
+    var selectedPalette = event.target.value
+    palette.href = '$PaletteCDN' + selectedPalette + '.css'
+}
+"@
+
+if (-not $script:AllPalettes) {
+    $script:AllPalettes = Invoke-RestMethod $AllPalettesSource
+}
+
+if (-not $script:Cache) {
+    $script:Cache = [Ordered]@{}
+}
+
+$paletteSelector = @"
+<style>
+.palette-controls {
+    display: flex;
+    flex-direction: row;
+    place-items: center;
+    justify-items: center;
+    justify-content: space-between;
+}
+.palette-controls * {
+    height: 2rem;
+}
+.invisible { display: none; }
+</style>
+<section class='palette-controls'>
+<select id='$SelectPaletteId' onchange='SetPalette(event)'>
+$(    
+    foreach ($palette in $script:AllPalettes.psobject.Properties) {
+        $paletteName = $palette.name
+        $palette = $palette.value
+        $selectedPalette = if ($defaultPalette -and $defaultPalette -eq $paletteName) { " selected='true'"} else { '' }
+        "<option data-luma='$($palette.luma)' value='$([Web.HttpUtility]::HtmlAttributeEncode($paletteName))'$selectedPalette>$([Web.HttpUtility]::HtmlEncode($paletteName))</option>"
+    }
+)
+</select>
+<button class='random-palette-button' command='--random-palette' id='random-palette-button' commandfor='$SelectPaletteId'>$(
+    /_includes/Feather -Icon refresh-cw
+)</button>
+
+</section>
+<script>
+document.getElementById('$selectPaletteId').addEventListener('command', (event)=> {
+    if (event.command == '--random-palette') {
+        var SelectPalette = event.target
+        var randomNumber = Math.floor(Math.random() * SelectPalette.length);
+        SelectPalette.selectedIndex = randomNumber
+        SetPalette(event)
+        return
+    }
+})
+</script>
+"@
+
+$HTML = @"
+<script>
+$JavaScript
+</script>
+$PaletteSelector
+"@
+
+$HTML
